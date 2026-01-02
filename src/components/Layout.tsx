edbca4bb942a49bb1c7ffbd5307a34d357fd97de
Tarun-Chandra <payakulatarunchandra@gmail.com>
2026-01-02 15:38:20 +0530

    added meta data

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -18,7 +18,26 @@ export default function Layout({ children, title = 'Student Resource Hub', descr
         <title>{title}</title>
         <meta name="description" content={description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="keywords" content="web development, engineering, coding, html, css, javascript, react, student resources, programming tutorials, learn to code" />
+        <meta name="author" content="Student Resource Hub" />
+        <meta name="robots" content="index, follow" />
+        <meta name="theme-color" content="#0f172a" />
         <link rel="icon" href="/logo.png" type="image/png" />
+
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://student-resource-hub-a758a.web.app/" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content="https://student-resource-hub-a758a.web.app/logo.png" />
+        <meta property="og:site_name" content="Student Resource Hub" />
+
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://student-resource-hub-a758a.web.app/" />
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={description} />
+        <meta property="twitter:image" content="https://student-resource-hub-a758a.web.app/logo.png" />
       </Head>
       <div className="min-h-screen flex flex-col">
         <Header />
